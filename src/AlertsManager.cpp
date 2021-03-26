a6446a91bfd17ff59be1953c7d8c246dfbf5d9f3
Simone Mainardi <mainardi@ntop.org>
2021-03-26 10:20:15 +0100

    Refactors alert keys into entity|id

--- src/AlertsManager.cpp
+++ src/AlertsManager.cpp
@@ -83,6 +83,8 @@ AlertsManager::AlertsManager(int interface_id, const char *filename) : StoreMana
   unlink(filePath);
   sprintf(&filePath[base_offset], "%s", "alerts_v25.db");
   unlink(filePath);
+  sprintf(&filePath[base_offset], "%s", "alerts_v26.db");
+  unlink(filePath);
   filePath[base_offset] = 0;
 
   /* open the newest */
