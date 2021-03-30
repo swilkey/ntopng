a313b13a10f8ab0c584d041c0c2f33f1f086e3a2
Simone Mainardi <mainardi@ntop.org>
2021-03-30 15:37:55 +0200

    Adds host_info to the generated alert JSON

--- src/HostAlert.cpp
+++ src/HostAlert.cpp
@@ -66,6 +66,7 @@ bool HostAlert::loadConfiguration(json_object *config) {
 /* ***************************************************** */
 
 ndpi_serializer* HostAlert::getSerializedAlert() {
+  char buf[64];
   ndpi_serializer *serializer = (ndpi_serializer *) malloc(sizeof(ndpi_serializer));
   
   if(serializer == NULL)
@@ -82,7 +83,27 @@ ndpi_serializer* HostAlert::getSerializedAlert() {
   ndpi_serialize_start_of_block(serializer, "alert_generation");
   ndpi_serialize_string_string(serializer, "script_key", getCallbackName().c_str());
   ndpi_serialize_string_string(serializer, "subdir", "host");
-  ndpi_serialize_end_of_block(serializer);
+
+  ndpi_serialize_start_of_block(serializer, "host_info");
+  
+  ndpi_serialize_string_string(serializer, "name", host->get_visual_name(buf, sizeof(buf)));
+  ndpi_serialize_string_boolean(serializer, "localhost", host->isLocalHost());
+  ndpi_serialize_string_boolean(serializer, "systemhost", host->isSystemHost());
+  ndpi_serialize_string_boolean(serializer, "privatehost", host->isPrivateHost());
+  ndpi_serialize_string_boolean(serializer, "broadcast_domain_host", host->isBroadcastDomainHost());
+  ndpi_serialize_string_boolean(serializer, "dhcpHost", host->isDhcpHost());
+  ndpi_serialize_string_boolean(serializer, "is_blacklisted", host->isBlacklisted());
+  ndpi_serialize_string_boolean(serializer, "is_broadcast", host->isBroadcastHost());
+  ndpi_serialize_string_boolean(serializer, "is_multicast", host->isMulticastHost());
+  
+#ifdef HAVE_NEDGE
+  ndpi_serialize_string_boolean(serializer, "childSafe", host->isChildSafe());
+  ndpi_serialize_string_boolean(serializer, "drop_all_host_traffic", host->dropAllTraffic());
+#endif
+
+
+  ndpi_serialize_end_of_block(serializer); /* host_info        */
+  ndpi_serialize_end_of_block(serializer); /* alert_generation */
   
   /* This call adds callback-specific information to the serializer */
   getAlertJSON(serializer);
