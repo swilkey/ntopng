ac5108f8a2911a151c2c22ea55660046b9321c51
Simone Mainardi <mainardi@ntop.org>
2021-04-01 14:42:33 +0200

    Removes include

--- include/ntop_includes.h
+++ include/ntop_includes.h
@@ -219,7 +219,6 @@ using namespace std;
 #include "ScoreStats.h"
 #include "ViewScoreStats.h"
 #include "Score.h"
-#include "HostScore.h"
 #include "FrequentStringItems.h"
 #include "Trace.h"
 #include "ProtoStats.h"
