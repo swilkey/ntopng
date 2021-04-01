d8c3eb737d129de87a500a27ac7c31716b94d804
Alfredo Cardigliano <cardigliano@ntop.org>
2021-04-01 14:49:53 +0200

    Restored alert_tcp_syn_flood_victim alert_tcp_syn_scan_victim definitions

--- scripts/lua/modules/alert_definitions/other/alert_tcp_syn_flood_victim.lua
+++ scripts/lua/modules/alert_definitions/other/alert_tcp_syn_flood_victim.lua
@@ -7,9 +7,10 @@
 local other_alert_keys = require "other_alert_keys"
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
+local json = require("dkjson")
 local alert_creators = require "alert_creators"
 local format_utils = require "format_utils"
-local json = require("dkjson")
+
 -- Import the classes library.
 local classes = require "classes"
 -- Make sure to import the Superclass!
@@ -17,13 +18,13 @@ local alert = require "alert"
 
 -- ##############################################
 
-local alert_flows_flood_victim = classes.class(alert)
+local alert_tcp_syn_flood_victim = classes.class(alert)
 
 -- ##############################################
 
-alert_flows_flood_victim.meta = {
-  alert_key = other_alert_keys.alert_flows_flood_victim,
-  i18n_title = "alerts_dashboard.flows_flood_victim",
+alert_tcp_syn_flood_victim.meta = {
+  alert_key = other_alert_keys.alert_tcp_syn_flood_victim,
+  i18n_title = "alerts_dashboard.tcp_syn_flood_victim",
   icon = "fas fa-life-ring",
   has_victim = true,
 }
@@ -34,7 +35,7 @@ alert_flows_flood_victim.meta = {
 -- @param one_param The first alert param
 -- @param another_param The second alert param
 -- @return A table with the alert built
-function alert_flows_flood_victim:init(metric, value, operator, threshold)
+function alert_tcp_syn_flood_victim:init(metric, value, operator, threshold)
    -- Call the parent constructor
    self.super:init()
 
@@ -48,21 +49,18 @@ end
 -- @param alert The alert description table, including alert data such as the generating entity, timestamp, granularity, type
 -- @param alert_type_params Table `alert_type_params` as built in the `:init` method
 -- @return A human-readable string
-function alert_flows_flood_victim.format(ifid, alert, alert_type_params)
-  local alert_consts = require("alert_consts")
+function alert_tcp_syn_flood_victim.format(ifid, alert, alert_type_params)
+  local alert_consts = require "alert_consts"
   local entity = alert_consts.formatAlertEntity(ifid, alert_consts.alertEntityRaw(alert["alert_entity"]), alert["alert_entity_val"])
-  local value = alert_type_params.value
-
-  if(value == nil) then value = 0 end
   
-  return i18n("alert_messages.flow_flood_victim", {
+  return i18n("alert_messages.syn_flood_victim", {
     entity = firstToUpper(entity),
     host_category = format_utils.formatAddressCategory((json.decode(alert.alert_json)).alert_generation.host_info),
-    value = string.format("%u", math.ceil(value)),
+    value = string.format("%u", math.ceil(alert_type_params.value)),
     threshold = alert_type_params.threshold,
   })
 end
 
 -- #######################################################
 
-return alert_flows_flood_victim
+return alert_tcp_syn_flood_victim
