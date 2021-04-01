5268a6c6409c132d920fcfec4af2cbdbe28522f9
Alfredo Cardigliano <cardigliano@ntop.org>
2021-04-01 14:03:24 +0200

    Engaged alert init

--- src/HostAlertableEntity.cpp
+++ src/HostAlertableEntity.cpp
@@ -24,6 +24,7 @@
 /* ****************************************** */
 
 HostAlertableEntity::HostAlertableEntity(NetworkInterface *iface, AlertEntity entity) : AlertableEntity(iface, entity) {
+  memset(engaged_alerts, 0, sizeof(engaged_alerts));
 }
 
 /* ****************************************** */
