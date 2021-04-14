7412465f31045f513512172d36a57776494d51f6
Alfredo Cardigliano <cardigliano@ntop.org>
2021-04-14 12:24:52 +0200

    Implement mac alerts store

--- scripts/lua/modules/alert_store/mac_alert_store.lua
+++ scripts/lua/modules/alert_store/mac_alert_store.lua
@@ -22,6 +22,7 @@ function mac_alert_store:new(args)
 
    -- Subclass using the base class instance
    self.key = "mac"
+   self._table_name = "mac_alerts"
 
    -- self is passed as argument so it will be set as base class metatable
    -- and this will actually make it possible to override functions
@@ -34,25 +35,28 @@ end
 -- ##############################################
 
 function mac_alert_store:insert(alert)
-   local table_name = "mac_alerts"
+   local is_attacker = false -- TODO
+   local is_victim = false -- TODO
 
-   traceError(TRACE_NORMAL, TRACE_CONSOLE, "mac_alert_store:insert")
-  
-   -- TODO
+   local insert_stmt = string.format("INSERT INTO %s "..
+      "(alert_id, tstamp, tstamp_end, severity, address, device_type, name, "..
+      "is_attacker, is_victim, json) "..
+      "VALUES (%u, %u, %u, %u, '%s', %u, '%s', %u, %u, '%s'); ",
+      self._table_name, 
+      alert.alert_type,
+      alert.alert_tstamp,
+      alert.alert_tstamp_end,
+      alert.alert_severity,
+      self:_escape(alert.alert_entity_val),
+      0, -- TODO device_type
+      self:_escape(""), -- TODO name
+      ternary(is_attacker, 1, 0),
+      ternary(is_victim, 1, 0),
+      self:_escape(alert.alert_json))
 
-   local json = alert.alert_json or ""
+   -- traceError(TRACE_NORMAL, TRACE_CONSOLE, insert_stmt)
 
-   local insert_stmt = "INSERT INTO "..table_name.."("..
-        "alert_id, "..
-
-      ") "..
-      "VALUES ("..
-        alert.alert_type..", "..
-
-        json..
-      "); "
-
-   --return interface.alert_store_query(insert_stmt)
+   return interface.alert_store_query(insert_stmt)
 end
 
 -- ##############################################
